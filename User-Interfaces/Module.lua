e7977b391ab8214701a52b1ef7facd55da2f0d86
derek <83593971+derek38@users.noreply.github.com>
2022-04-28 12:53:49 -0400

    Update Module.lua

--- User-Interfaces/Module.lua
+++ User-Interfaces/Module.lua
@@ -1,4 +1,4 @@
-local Utility = loadstring(game:HttpGet("https://github.com/derek38/Private/blob/main/Scripts/Utility.lua"))()
+local Utility = loadstring(game:HttpGet("https://raw.githubusercontent.com/derek38/Private/main/Scripts/Utility.lua"))()
 
 local CoreGui = game:GetService("CoreGui")
 local PlayerService = game:GetService("Players")
